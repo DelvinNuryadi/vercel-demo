46a34b5ff56fcc3668a3d493a64897b51a830682
Delvin <me@delvinuryadi.my.id>
2025-01-09 16:56:15 +0800

    update

--- api/main.py
+++
@@ -1,6 +0,0 @@
-from fastapi import fastAPI
-app = FastAPI()
-
-@app.get("/")
-async def health_check():
-    return "the health check is successfull!"
---
+++ main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+app = FastAPI()
+
+@app.get("/")
+async def home_root():
+    return {"message": "success"}
+
+@app.get("/deploy")
+async def home_root():
+    return {"message":"vercel deployment"}
